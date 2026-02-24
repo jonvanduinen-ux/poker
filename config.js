d94e7ec3d0a1c320f5a7312c1f9b3f6db13fa1af
jonvanduinen-ux <jon.van.duinen@gmail.com>
2026-02-24 13:21:06 -0500

    Create config.js

---
+++ config.js
@@ -0,0 +1,4 @@
+window.POKER_CONFIG = window.POKER_CONFIG || {
+  supabaseUrl: "https://webiuzbeadhcpkrfnhcu.supabase.co",
+  supabaseAnonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndlYml1emJlYWRoY3BrcmZuaGN1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzE5NTM2NjksImV4cCI6MjA4NzUyOTY2OX0.gNOFpA6o7JjEpPYhMl6Owp3c9pemf5d7BUTu2HV5J7s"
+};
